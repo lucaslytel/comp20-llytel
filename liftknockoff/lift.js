006b1946ce1e9480bdebad513fdb42c1a01b5827
Lucas Lytel <lucaslytel@Lucass-MacBook-Pro.local>
2019-03-05 16:09:25 -0500

    almost working displaymarkers function still doesn't work

--- liftknockoff/lift.js
+++ liftknockoff/lift.js
@@ -6,11 +6,13 @@ var myOptions = {
  zoom: 13,
  center: me, };
 var marker;
-// var infowindow = new google.maps.infowindow();
+var infowindow = new google.maps.InfoWindow();
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
     getMyLocation();
 }
+
 function getMyLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -24,6 +26,7 @@ function getMyLocation() {
 			alert("Geolocation is not supported by your browser. Sad.");
 		}
 }
+
 function renderMap() {
 	me = new google.maps.LatLng(myLat,myLng);
 	map.panTo(me)
@@ -32,15 +35,17 @@ function renderMap() {
 		title: "Here is me!"
 	});
 	marker.setMap(map);
-//	google.maps.event.addListener(marker,'click', function() {
-//		infowindow.setContent(this.title);
-//		inforwindow.open(map,this);
-//	});
+	/*google.maps.event.addListener(marker,'click', function() {
+		infowindow.setContent(this.title);
+		inforwindow.open(map,this);
+	});*/
 }
+
 function userdata() {
 	var request = new XMLHttpRequest();
 	var data = 'username=ajNnfhJj&lat='+myLat+'&lng='+myLng;
 	var url = 'https://hans-moleman.herokuapp.com/rides';
+	
 	request.open('POST', url, true)
 	
 	request.setRequestHeader('Content-type','application/x-www-form-urlencoded')
@@ -54,16 +59,46 @@ function userdata() {
 	}
 	request.send(data);
 }
-function displaymarkers(userdata) {
-	if (userdata.passangers) 
-		var img = new Image (){
-	for (i = 0; i < userdata.passangers.length, i++;) {
-		if (userdata.passangers.username == WEINERMOBILE) {
-			// display distance from WEINERMOBILE if true
-		}
-		var marker = new google.maps.Marker 
-			position: userdata.lng, userdata.lat;
 
-	}
+function displaymarkers(userdata) {
+	/*console.log(userdata);
+	var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+	var icons = {
+		passanger: {
+			icon: 'passanger.png'
+		},
+		driver: {
+			icon: 'car.png'
+		},
+		weinermobile: {
+			icon: 'weinermobile.png'
+		}
+	}*/
+	if (userdata.passangers) { 
+		console.log(userdata);
+		for (i = 0; i < userdata.passangers.length, i++;) {
+			if (userdata.passangers[i].username == WEINERMOBILE) {
+				var marker = new google.maps.Marker ({
+					position: google.maps.LatLng(userdata.passanger.lat, userdata.passanger.lng),
+					title: "WEINERMOBILE",
+					icon: icon[weinerboible].icon
+				});
+			}
+			var marker = new google.maps.Marker ({
+				position: google.maps.LatLng(userdata.passanger.lat, userdata.passanger.lng),
+				title: userdata.id,
+				icon: icons[passanger.icon].icon
+			});
+		}
+	} else if (userdata.vehicles) {
+		for (i = 0; i < userdata.vehicles.length, i++;) {
+			if(userdata.vehicles[i].username == WEINERMOBILE) {
+				var marker = new google.maps.Marker ({
+					position: google.maps.LatLng(userdata.vehicles.lat, userdata.vehicles.lng),
+					title: "WEINERMOBILE",
+					icon: icon[weinermobile].icon
+				});
+			}
+		}
 	}
 }
